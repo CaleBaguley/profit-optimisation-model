94a54e689125bd4640f8e9b79193b8beca9335bf
CaleBaguley <jb14389@bristol.ac.uk>
2023-10-23 10:10:35 +0100

    increased precision of MOLAR_GAS_CONSTANT.

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@
 Fundamental constants.
 """
 
-MOLAR_GAS_CONSTANT: float = 8.314                  # J K^-1 mol^-1
+MOLAR_GAS_CONSTANT: float = 8.3144598              # J K^-1 mol^-1
 LATENT_HEAT_OF_WATER: float = 2.501E6              # J kg-1
 MOLAR_MASS_OF_WATER: float = 18E-3                 # kg mol-1
 GAS_CONSTANT_OF_DRY_AIR = 287.058                  # Jkg-1 K-1
