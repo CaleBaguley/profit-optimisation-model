9a000ebdcdc5e453e5d75fa005f2284e09ae8f79
CaleBaguley <jb14389@bristol.ac.uk>
2023-10-25 11:57:37 +0100

    created photosynthesis model using equations from Leuning et al 1990.

---
+++ src/PhotosynthesisModels/Leuning_Model.py
@@ -0,0 +1,183 @@
+import numpy as np
+from numpy import roots, nanmax
+
+from src.PhotosynthesisModels.photosynthesis_model import PhotosynthesisModelDummy
+from src.TemperatureDependenceModels.Q10_temperature_dependence_model import Q10TemperatureDependenceModel
+from src.TemperatureDependenceModels.arrhenius_and_peaked_arrhenius_function import ArrheniusModel
+from src.TemperatureDependenceModels.temperature_dependence_model import TemperatureDependenceModel
+from src.electron_transport_rate_model import ElectronTransportRateModel
+from src.rubisco_CO2_and_O_model import RubiscoRates
+
+
+class PhotosynthesisModelRubiscoLimitedLeuning(PhotosynthesisModelDummy):
+
+    _rubisco_rates_model: RubiscoRates
+    _CO2_compensation_point_model: TemperatureDependenceModel
+    _mitochondrial_respiration_rate_model: TemperatureDependenceModel
+
+    def __init__(self,
+                 rubisco_rates_model=RubiscoRates(),
+                 CO2_compensation_point_model=ArrheniusModel(42.75, 37830.0),
+                 mitochondrial_respiration_rate_model=Q10TemperatureDependenceModel(0.2, 2.)):
+        self._rubisco_rates_model = rubisco_rates_model
+        self._CO2_compensation_point_model = CO2_compensation_point_model
+        self._mitochondrial_respiration_rate_model = mitochondrial_respiration_rate_model
+
+    def intercellular_CO2_concentration(self,
+                                        stomatal_conductance_to_CO2,
+                                        atmospheric_CO2_concentration,
+                                        leaf_temperature,
+                                        intercellular_O = None,
+                                        utilized_photosynthetically_active_radiation = None):
+        """
+
+
+        @param stomatal_conductance_to_CO2: mol m-2 s-1
+        @param atmospheric_CO2_concentration: umol mol-1
+        @param leaf_temperature: K
+        @param intercellular_O: umol mol-1
+        @param utilized_photosynthetically_active_radiation: Not needed
+        @return: intercellular CO2 concentration (umol mol-1)
+        """
+
+        mitochondrial_respiration_rate = (
+            self._mitochondrial_respiration_rate_model.get_value_at_temperature(leaf_temperature))
+
+        michaelis_menten_constant_carboxylation = (
+            self._rubisco_rates_model.michaelis_menten_constant_carboxylation(leaf_temperature, intercellular_O))
+
+        maximum_carboxylation_rate = self._rubisco_rates_model.maximum_carboxylation_rate(leaf_temperature)
+
+        mitochondrial_respiration_rate = 0.015*maximum_carboxylation_rate
+
+        CO2_compensation_point = self._CO2_compensation_point_model.get_value_at_temperature(leaf_temperature)
+
+        #print('\nRubisco limited:')
+        #print('stomatal conductance to CO2: ', stomatal_conductance_to_CO2)
+        #print('atmospheric CO2 concentration: ', atmospheric_CO2_concentration)
+        #print('mitochondrial respiration rate: ', mitochondrial_respiration_rate)
+        #print('michaelis menten constant carboxylation: ', michaelis_menten_constant_carboxylation)
+        #print('maximum carboxylation rate: ', maximum_carboxylation_rate)
+        #print('CO2 compensation point: ', CO2_compensation_point)
+
+        # Quadratic equation components (Ax^2 + Bx + C = 0)
+        A = -stomatal_conductance_to_CO2
+
+        B = ((atmospheric_CO2_concentration - michaelis_menten_constant_carboxylation) * stomatal_conductance_to_CO2
+             - maximum_carboxylation_rate
+             + mitochondrial_respiration_rate)
+
+        C = (michaelis_menten_constant_carboxylation * atmospheric_CO2_concentration * stomatal_conductance_to_CO2
+             + maximum_carboxylation_rate * CO2_compensation_point
+             + mitochondrial_respiration_rate * michaelis_menten_constant_carboxylation)
+
+        # Find the roots of the quadratic equation
+        intercellular_CO2_concentration = roots([A, B, C])
+
+        #print("roots: ", intercellular_CO2_concentration)
+        #print("Manon roots: ", [quadratic(a=A, b=B, c=C, large=True), quadratic(a=A, b=B, c=C, large=False)])
+
+        intercellular_CO2_concentration = nanmax(intercellular_CO2_concentration)
+
+        if(intercellular_CO2_concentration is None):
+            return np.nan
+        elif (intercellular_CO2_concentration < 0. or intercellular_CO2_concentration > atmospheric_CO2_concentration):
+            return np.nan
+
+        return intercellular_CO2_concentration
+        #return max(intercellular_CO2_concentration)
+
+
+class PhotosynthesisModelElectronTransportLimitedLeuning(PhotosynthesisModelDummy):
+
+    _electron_transport_rate_model: ElectronTransportRateModel
+    _CO2_compensation_point_model: TemperatureDependenceModel
+    _mitochondrial_respiration_rate_model: TemperatureDependenceModel
+    _rubisco_rates_model: RubiscoRates
+
+    def __init__(self,
+                 electron_transport_rate_model = ElectronTransportRateModel(),
+                 CO2_compensation_point_model = ArrheniusModel(42.75, 37830.0),
+                 mitochondrial_respiration_rate_model = Q10TemperatureDependenceModel(0.2, 2.),
+                 rubisco_rates_model=RubiscoRates()
+                 ):
+        """
+
+        @param electron_transport_rate_model:
+        @param CO2_compensation_point_model:
+        @param mitochondrial_respiration_rate_model:
+        @param rubisco_rates_model:
+        """
+
+        self._electron_transport_rate_model = electron_transport_rate_model
+        self._CO2_compensation_point_model = CO2_compensation_point_model
+        self._mitochondrial_respiration_rate_model = mitochondrial_respiration_rate_model
+        self._rubisco_rates_model = rubisco_rates_model
+
+    def intercellular_CO2_concentration(self,
+                                        stomatal_conductance_to_CO2,
+                                        atmospheric_CO2_concentration,
+                                        leaf_temperature,
+                                        intercellular_O = None,
+                                        utilized_photosynthetically_active_radiation = None):
+        """
+
+        @param stomatal_conductance_to_CO2: mol m-2 s-1
+        @param atmospheric_CO2_concentration: umol mol-1
+        @param leaf_temperature: K
+        @param intercellular_O: umol mol-1
+        @param utilized_photosynthetically_active_radiation: (umol m-2 unit time-1)
+        @return: intercellular CO2 concentration (umol mol-1)
+        """
+        #print('\nElectron transport rate limited:')
+
+        if(utilized_photosynthetically_active_radiation == 0.):
+            return atmospheric_CO2_concentration
+
+        maximum_carboxylation_rate = self._rubisco_rates_model.maximum_carboxylation_rate(leaf_temperature)
+
+        mitochondrial_respiration_rate = 0.015 * maximum_carboxylation_rate
+
+        #mitochondrial_respiration_rate = (
+        #    self._mitochondrial_respiration_rate_model.get_value_at_temperature(leaf_temperature))
+
+        electron_transport_rate = (
+            self._electron_transport_rate_model.electron_transport_rate(leaf_temperature,
+                                                                        utilized_photosynthetically_active_radiation))
+
+        CO2_compensation_point = self._CO2_compensation_point_model.get_value_at_temperature(leaf_temperature)
+
+
+        #print('stomatal conductance to CO2: ', stomatal_conductance_to_CO2)
+        #print('atmospheric CO2 concentration: ', atmospheric_CO2_concentration)
+        #print('mitochondrial respiration rate: ', mitochondrial_respiration_rate)
+        #print('maximum carboxylation rate: ', maximum_carboxylation_rate)
+        #print('electron transport rate: ', electron_transport_rate)
+        #print('CO2 compensation point: ', CO2_compensation_point)
+
+        # Quadratic equation components (Ax^2 + Bx + C = 0)
+        A = -stomatal_conductance_to_CO2
+
+        B = ((atmospheric_CO2_concentration - 2*CO2_compensation_point) * stomatal_conductance_to_CO2
+             - electron_transport_rate
+             + mitochondrial_respiration_rate)
+
+        C = (2*CO2_compensation_point * atmospheric_CO2_concentration * stomatal_conductance_to_CO2
+             + electron_transport_rate * CO2_compensation_point
+             + mitochondrial_respiration_rate * 2*CO2_compensation_point)
+
+        # Find the roots of the quadratic equation
+        intercellular_CO2_concentration = roots([A, B, C])
+
+        #print("roots: ", intercellular_CO2_concentration)
+        #print("Manon roots: ", [quadratic(a=A, b=B, c=C, large=True), quadratic(a=A, b=B, c=C, large=False)])
+
+        intercellular_CO2_concentration = nanmax(intercellular_CO2_concentration)
+
+        if (intercellular_CO2_concentration is None):
+            return np.nan
+        elif (intercellular_CO2_concentration < 0. or intercellular_CO2_concentration > atmospheric_CO2_concentration):
+            return np.nan
+
+        return intercellular_CO2_concentration
+        #return max(intercellular_CO2_concentration)
