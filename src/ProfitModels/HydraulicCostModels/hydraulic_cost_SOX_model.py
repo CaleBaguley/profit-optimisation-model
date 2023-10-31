c52237a7bac8f9267ed335b29f651908273ba7f1
CaleBaguley <jb14389@bristol.ac.uk>
2023-10-31 14:44:24 +0000

    Removed duplicate code.

--- src/ProfitModels/HydraulicCostModels/hydraulic_cost_SOX_model.py
+++ src/ProfitModels/HydraulicCostModels/hydraulic_cost_SOX_model.py
@@ -8,16 +8,7 @@ from src.ProfitModels.HydraulicCostModels.hydraulic_cost_model import HydraulicC
 from src.HydraulicConductanceModels.hydraulic_conductance_model import HydraulicConductanceModel
 
 
-class ProfitMaxHydraulicCostModel(HydraulicCostModel):
-    _curvature_parameter: float
-
-    def __init__(self,
-                 hydraulic_conductance_model: HydraulicConductanceModel,
-                 critical_leaf_water_potential: float,
-                 curvature_parameter: float):
-
-        super().__init__(hydraulic_conductance_model, critical_leaf_water_potential)
-        self._curvature_parameter = curvature_parameter
+class SOXHydraulicCostModel(HydraulicCostModel):
 
     def hydraulic_cost(self, leaf_water_potential, soil_water_potential):
         """
