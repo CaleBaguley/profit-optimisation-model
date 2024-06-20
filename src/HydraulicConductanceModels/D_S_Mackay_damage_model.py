a8867b9ed9ab0b8c90896cd74923e26db6086472
CaleBaguley <jb14389@bristol.ac.uk>
2024-06-20 11:45:39 +0100

    Added function to create DSMackayXylemDamageModel object from two pairs of water potentials and conductance loss fractions.

--- src/HydraulicConductanceModels/D_S_Mackay_damage_model.py
+++ src/HydraulicConductanceModels/D_S_Mackay_damage_model.py
@@ -7,8 +7,10 @@ Implimentation of the model of xylem damage by embolism as described by D.S.Mack
 
 from numpy import exp, linspace, asarray, clip, sum
 from scipy.optimize import leastsq
-from src.HydraulicConductanceModels.cumulative_Weibull_distribution_model import (cumulative_Weibull_distribution,
-                                                                                  CumulativeWeibullDistribution)
+from src.HydraulicConductanceModels.cumulative_Weibull_distribution_model \
+    import (cumulative_Weibull_distribution,
+            CumulativeWeibullDistribution,
+            cumulative_Weibull_distribution_parameters_from_conductance_loss)
 
 class DSMackayXylemDamageModel(CumulativeWeibullDistribution):
 
@@ -85,52 +87,37 @@ class DSMackayXylemDamageModel(CumulativeWeibullDistribution):
 
         return True
 
-
-
-def D_S_Mackay_damage_model(new_k_max, current_CW_model, sample_points = 1000):
+def D_S_Mackay_damage_model_from_conductance_loss(maximum_conductance,
+                                                  water_potential_1,
+                                                  water_potential_2,
+                                                  conductance_loss_fraction_1,
+                                                  conductance_loss_fraction_2,
+                                                  N_sample_points_xylem_damage = 1000,
+                                                  critical_conductance_loss_fraction = 0.9,
+                                                  PLC_damage_threshold = 0.05):
 
     """
-    Create a new cumulative Weibull distribution model from the existing
-    model and the new k_max value.
-
-    @param new_k_max: float
-    @param current_CW_model: CumulativeWeibullDistribution
-    @param sample_points: number of water potentials to use when fitting
-                          the shape parameter c; int
-    @return: new CumulativeWeibullDistribution
+    @param maximum_conductance:
+    @param water_potential_1: MPa
+    @param water_potential_2: MPa
+    @param conductance_loss_fraction_1: unitless
+    @param conductance_loss_fraction_2: unitless
+    @param N_sample_points_xylem_damage: int
+    @param critical_conductance_loss_fraction: unitless
+    @param PLC_damage_threshold: unitless
+    @return: DSMackayXylemDamageModel
     """
 
-    # find the new sensitivity parameter. This is the water potential at which the
-    # conductance of the current model is equal to the new k_max value times e to
-    # the minus one.
-    b_new = current_CW_model.water_potential_from_conductance(new_k_max * exp(-1))
+    maximum_conductance, sensitivity_parameter, shape_parameter = \
+        cumulative_Weibull_distribution_parameters_from_conductance_loss(maximum_conductance,
+                                                                         water_potential_1,
+                                                                         water_potential_2,
+                                                                         conductance_loss_fraction_1,
+                                                                         conductance_loss_fraction_2)
 
-    # Setup the interim capped conductance model
-    psi_array = linspace(0, current_CW_model.critical_water_potential, sample_points)
-    capped_conductance_array = asarray([current_CW_model.conductance(psi) for psi in psi_array])
-    capped_conductance_array = clip(capped_conductance_array, 0, new_k_max)
-
-    # Calculate the conductivity loss P of the capped conductance model at each
-    # water potential.
-    P_array = 1 - capped_conductance_array / new_k_max
-
-    # Fit score to minimise as a function to pass to the curve_fit function
-    def fit_score(c_current):
-        k_prime = cumulative_Weibull_distribution(psi_array, new_k_max, b_new, c_current)
-
-        numerator = sum(P_array*k_prime)**2
-        denominator = (sum(P_array)**2)*(sum(k_prime)**2)
-
-        return numerator/denominator
-
-    # Fit the shape parameter c to the capped conductance model
-    c_new = leastsq(func = fit_score,
-                    x0 = current_CW_model.shape_parameter)[0][0]
-
-    # Update critical conductance loss fraction. This maintains the critical conductance value
-    critical_conductance_loss_fraction = current_CW_model.critical_conductance / new_k_max
-
-    return CumulativeWeibullDistribution(new_k_max,
-                                         b_new,
-                                         c_new,
-                                         critical_conductance_loss_fraction)
+    return DSMackayXylemDamageModel(maximum_conductance,
+                                    sensitivity_parameter,
+                                    shape_parameter,
+                                    N_sample_points_xylem_damage,
+                                    critical_conductance_loss_fraction,
+                                    PLC_damage_threshold)
