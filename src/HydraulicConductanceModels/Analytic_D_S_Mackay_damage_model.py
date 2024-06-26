1117737673c4641575b1ad979540744cab09413b
CaleBaguley <jb14389@bristol.ac.uk>
2024-06-26 13:37:00 +0100

    Updated method to create model from P50 and P88 values.

--- src/HydraulicConductanceModels/Analytic_D_S_Mackay_damage_model.py
+++ src/HydraulicConductanceModels/Analytic_D_S_Mackay_damage_model.py
@@ -8,6 +8,9 @@ Analytic implementation of the model of xylem damage by embolism as described by
 from numpy import exp, power
 from src.HydraulicConductanceModels.D_S_Mackay_damage_model \
     import DSMackayXylemDamageModel
+from src.HydraulicConductanceModels.cumulative_Weibull_distribution_model \
+    import cumulative_Weibull_distribution_parameters_from_conductance_loss
+
 
 class DSMackayXylemDamageModelAnalytic(DSMackayXylemDamageModel):
 
@@ -28,7 +31,7 @@ class DSMackayXylemDamageModelAnalytic(DSMackayXylemDamageModel):
                          xylem_recovery_water_potnetial,
                          PLC_damage_threshold)
 
-    def damage_xylem(self, water_potential, timestep, transpiration_rate):
+    def _damage_xylem(self, water_potential, timestep, transpiration_rate):
 
         """
         @param water_potential: (MPa)
@@ -51,7 +54,7 @@ class DSMackayXylemDamageModelAnalytic(DSMackayXylemDamageModel):
 
         new_c = (self._shape_parameter
                  * power(b_new/self._sensitivity_parameter, self._shape_parameter)
-                 * (new_k_max / self._k_max)
+                 * (self._k_max / new_k_max)
                  * exp(exponent))
 
         self._k_max = new_k_max
@@ -59,3 +62,42 @@ class DSMackayXylemDamageModelAnalytic(DSMackayXylemDamageModel):
         self._shape_parameter = new_c
 
         return True
+
+
+def analytic_D_S_Mackay_damage_model_from_conductance_loss(maximum_conductance,
+                                                           water_potential_1,
+                                                           water_potential_2,
+                                                           conductance_loss_fraction_1,
+                                                           conductance_loss_fraction_2,
+                                                           N_sample_points_xylem_damage = 1000,
+                                                           critical_conductance_loss_fraction = 0.9,
+                                                           xylem_recovery_water_potnetial = 0.,
+                                                           PLC_damage_threshold = 0.05):
+
+    """
+    @param maximum_conductance:
+    @param water_potential_1: MPa
+    @param water_potential_2: MPa
+    @param conductance_loss_fraction_1: unitless
+    @param conductance_loss_fraction_2: unitless
+    @param N_sample_points_xylem_damage: int
+    @param critical_conductance_loss_fraction: unitless
+    @param xylem_recovery_water_potnetial: MPa
+    @param PLC_damage_threshold: unitless
+    @return: DSMackayXylemDamageModel
+    """
+
+    maximum_conductance, sensitivity_parameter, shape_parameter = \
+        cumulative_Weibull_distribution_parameters_from_conductance_loss(maximum_conductance,
+                                                                         water_potential_1,
+                                                                         water_potential_2,
+                                                                         conductance_loss_fraction_1,
+                                                                         conductance_loss_fraction_2)
+
+    return DSMackayXylemDamageModelAnalytic(maximum_conductance,
+                                            sensitivity_parameter,
+                                            shape_parameter,
+                                            N_sample_points_xylem_damage,
+                                            critical_conductance_loss_fraction,
+                                            xylem_recovery_water_potnetial,
+                                            PLC_damage_threshold)
