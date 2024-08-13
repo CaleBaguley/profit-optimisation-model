4f047e577ae03806d2a034025c8420b2255a886d
CaleBaguley <jb14389@bristol.ac.uk>
2024-08-13 15:24:37 +0100

    Added functions to create conductance model from two conductance water potential pairs.

--- profit_optimisation_model/src/HydraulicConductanceModels/SOX_hydraulic_conductance_model.py
+++ profit_optimisation_model/src/HydraulicConductanceModels/SOX_hydraulic_conductance_model.py
@@ -4,7 +4,7 @@
 -----------------------------------------------------------------------------------------
 """
 
-from numpy import power
+from numpy import power, log
 from src.HydraulicConductanceModels.hydraulic_conductance_model import HydraulicConductanceModel
 
 
@@ -54,3 +54,51 @@ class SOXHydraulicConductanceModel(HydraulicConductanceModel):
 
         return self._water_potential_at_half_conductance * power(1/conductance_fraction - 1,
                                                                  1/self._shape_parameter)
+
+# -- Model creation functions ------------------------------------------------
+
+
+def SOX_conductance_model_from_conductance_loss_at_goven_water_potentials(
+    maximum_conductance,
+    water_potential_1,
+    water_potential_2,
+    conductance_loss_fraction_1,
+    conductance_loss_fraction_2,
+    critical_conductance_loss_fraction = 0.9,
+    xylem_recovery_water_potnetial = 0.,
+    PLC_damage_threshold = 0.1
+    ):
+
+    maximum_conductance, water_potential_at_half_conductance, shape_parameter = (
+        SOX_conductance_parameters_from_conductance_loss_at_goven_water_potentials(
+            maximum_conductance,
+            water_potential_1,
+            water_potential_2,
+            conductance_loss_fraction_1,
+            conductance_loss_fraction_2
+            )
+        )
+
+    return SOXHydraulicConductanceModel(maximum_conductance,
+                                        water_potential_at_half_conductance,
+                                        shape_parameter,
+                                        critical_conductance_loss_fraction,
+                                        xylem_recovery_water_potnetial
+                                        )
+
+
+def SOX_conductance_parameters_from_conductance_loss_at_goven_water_potentials(
+    maximum_conductance,
+    water_potential_1,
+    water_potential_2,
+    conductance_loss_fraction_1,
+    conductance_loss_fraction_2,
+    ):
+
+    shape_parameter = (log(1/conductance_loss_fraction_1 - 1) - log(1/conductance_loss_fraction_2 - 1)
+                       / (log(water_potential_1) - log(water_potential_2)))
+
+    water_potential_at_half_conductance = (water_potential_1
+                                           * power(1/conductance_loss_fraction_1 - 1, -1/shape_parameter))
+
+    return maximum_conductance, water_potential_at_half_conductance, shape_parameter
